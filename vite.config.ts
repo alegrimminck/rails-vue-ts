cf8c9cdc38d793dadfa4f60165cf523c0e91f0f1
Alejandro Grimminck <alegrimminck@gmail.com>
2026-01-27 08:05:55 -0300

    chore(rails): add base stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import ruby from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
     ruby(),
     vue(),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./app/javascript', import.meta.url)),
+    },
+  },
 })
